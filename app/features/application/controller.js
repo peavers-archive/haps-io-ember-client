703a9277852e37ab581ff5cb643bc0c0424685e8
Chris Turner <chris@forloop.space>
2019-06-06 18:35:44 +1200

    cleaned: app/features/application/controller.js

--- app/features/application/controller.js
+++ app/features/application/controller.js
@@ -1,6 +1,6 @@
-import Controller from '@ember/controller';
-import {inject as service} from '@ember/service'
-import {get} from '@ember/object'
+import Controller from "@ember/controller";
+import { inject as service } from "@ember/service";
+import { get } from "@ember/object";
 
 export default Controller.extend({
   session: service(),
@@ -10,34 +10,32 @@ export default Controller.extend({
     login() {
       const lockOptions = {
         theme: {
-          logo: 'https://haps.io/assets/favicon-4bffc8bcbd4a4850e47bb9de7db7a61a.png',
-          primaryColor: '#757ffc',
+          logo:
+            "https://haps.io/assets/favicon-4bffc8bcbd4a4850e47bb9de7db7a61a.png",
+          primaryColor: "#757ffc"
         },
-
 
         languageDictionary: {
           title: "haps.io login"
-
         },
         autoclose: true,
 
-
         closable: true,
         auth: {
-
-
-        responseType: 'token id_token',
+          responseType: "token id_token",
           params: {
-
-            scope: 'openid profile email user_metadata',
-          },
+            scope: "openid profile email user_metadata"
+          }
         }
       };
-      get(this, 'session').authenticate('authenticator:auth0-lock', lockOptions);
+      get(this, "session").authenticate(
+        "authenticator:auth0-lock",
+        lockOptions
+      );
     },
 
     logout() {
-      get(this, 'session').invalidate();
+      get(this, "session").invalidate();
     }
   }
 });
