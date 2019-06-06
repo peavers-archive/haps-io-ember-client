f48ec1d30d48efe954a51358e2a71cfc399a07d1
Chris Turner <chris@forloop.space>
2019-06-06 17:05:41 +1200

    code format v2

--- tests/integration/features/components/feed-item/component-test.js
+++ tests/integration/features/components/feed-item/component-test.js
@@ -1,18 +1,22 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+import { moduleForComponent, test } from "ember-qunit";
+import hbs from "htmlbars-inline-precompile";
 
-moduleForComponent('feed-item', 'Integration | Component | feed item', {
+moduleForComponent("feed-item", "Integration | Component | feed item", {
   integration: true
 });
 
-test('it renders', function(assert) {
-
+test("it renders", function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
   this.render(hbs`{{feed-item}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(
+    this.$()
+      .text()
+      .trim(),
+    ""
+  );
 
   // Template block usage:
   this.render(hbs`
@@ -21,5 +25,10 @@ test('it renders', function(assert) {
     {{/feed-item}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(
+    this.$()
+      .text()
+      .trim(),
+    "template block text"
+  );
 });
