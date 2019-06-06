b37b4feaf2a1aceb1264cfcba9c6e95d61b1c83c
Chris Turner <chris@forloop.space>
2019-06-06 17:50:10 +1200

    code format v2

--- app/features/news/route.js
+++ app/features/news/route.js
@@ -1,36 +1,35 @@
-import Route from '@ember/routing/route';
+import Route from "@ember/routing/route";
 import RSVP from "rsvp";
 import InfinityRoute from "ember-infinity/mixins/route";
-import {get} from "@ember/object";
-import {inject as service} from '@ember/service';
+import { get } from "@ember/object";
+import { inject as service } from "@ember/service";
 
 export default Route.extend(InfinityRoute, {
-
   session: service(),
 
   model() {
-    const userEmail = get(this, 'session.data.authenticated.profile.email');
+    const userEmail = get(this, "session.data.authenticated.profile.email");
     const infinity = this.infinityModel("newsItem", {
       perPage: 10,
       startingPage: 0,
-      modelPath: 'controller.newsItems',
+      modelPath: "controller.newsItems",
       userId: userEmail,
-      
+
       archive: false
     });
 
-    return userEmail ? RSVP.hash({
-      user: get(this, 'store').queryRecord('user', {email: userEmail}),
-      
-      newsItems: infinity
-    }) : RSVP.hash({
-      newsItems: infinity
-    });
+    return userEmail
+      ? RSVP.hash({
+          user: get(this, "store").queryRecord("user", { email: userEmail }),
+
+          newsItems: infinity
+        })
+      : RSVP.hash({
+          newsItems: infinity
+        });
   },
 
   setupController(controller, models) {
     controller.setProperties(models);
-  },
-
-
+  }
 });
