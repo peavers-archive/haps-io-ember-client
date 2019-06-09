0d150d5ea133f3d4a88aa2e53b3a1fb39d4c9fbe
byteclean.io <no-reply@byteclean.io>
2019-06-09 03:28:47 +0000

    byteclean: app/features/application/adapter.js

--- app/features/application/adapter.js
+++ app/features/application/adapter.js
@@ -2,24 +2,5 @@ import DS from "ember-data";
 import config from "../../config/environment";
 
 export default DS.JSONAPIAdapter.extend({
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   host: config.APP.HAPS
 });
